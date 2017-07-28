c3faaaed4b386f1e2982c3e221a792c44cb9db6e
Ross Cooper <rosscooper@Gabriels-MacBook-Air.local>
2017-07-28 11:46:15 -0700

    Almost there, need to style, but pretty much done

--- app.js
+++ app.js
@@ -2,15 +2,25 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const mustacheExpress = require('mustache-express');
+const data = require('./data')
 
 app.engine('mustache', mustacheExpress());
+app.use(express.static(path.join(__dirname, 'static')))
 app.set('views', './views')
 app.set('view engine', 'mustache')
 
-app.use(express.static(path.join(__dirname, 'static')))
+
 
 app.get("/", function(req, res, next){
-  res.render("index", {appType:"Express"})
+	res.render("index", data)
+})
+
+app.get("/user/:username", function(req, res, next){
+	const user = data.users.filter(function(person){
+		return person.username === req.params.username
+	})[0]
+
+	res.render("user", user)
 })
 
 app.listen(3000, function(){
